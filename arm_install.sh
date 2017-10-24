f828447965581146ea2646f59c7e59249169e99a
Xrat Zhou <xratzh@gmail.com>
2017-10-24 12:47:25 +0800

    Update arm_install.sh

--- arm_install.sh
+++ arm_install.sh
@@ -16,9 +16,9 @@ echo "11: zhejiang_qiye_sxplugin.so"
 echo "12: zhejiang_xiaoyuan_sxplugin.so"  
 echo "请在下方空白处输入编号数字并回车以确认："
 echo "或者ctrl+c退出"
-echo "下载tar集合“
-curl -L https://github.com/xratzh/hiwifi-netkeeper/releases/download/v1.0/arm2.4.7.tar -o arm2.4.7.tar
-tar –xvf arm2.4.7.tar
+echo "下载tar集合"
+curl -k -L https://github.com/xratzh/hiwifi-netkeeper/releases/download/v1.0/arm2.4.7.tar -o arm2.4.7.tar
+tar xvf arm2.4.7.tar
 read so
 if [ $so = 1 ]
 then
