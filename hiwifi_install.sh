4653288dae6984cb6767545ed864a5e285ea8f3e
Xrat Zhou <xratzh@gmail.com>
2017-10-24 12:46:59 +0800

    Update hiwifi_install.sh

--- hiwifi_install.sh
+++ hiwifi_install.sh
@@ -16,9 +16,9 @@ echo "11: zhejiang_qiye_sxplugin.so"
 echo "12: zhejiang_xiaoyuan_sxplugin.so"  
 echo "请在下方空白处输入编号数字并回车以确认："
 echo "或者ctrl+c退出"
-echo "下载tar集合“
-curl -L https://github.com/xratzh/hiwifi-netkeeper/releases/download/v1.0/mipsel2.4.5.tar -o mipsel2.4.5.tar
-tar –xvf mipsel2.4.5.tar
+echo "下载tar集合"
+curl -k -L https://github.com/xratzh/hiwifi-netkeeper/releases/download/v1.0/mipsel2.4.5.tar -o mipsel2.4.5.tar
+tar xvf mipsel2.4.5.tar
 read so
 if [ $so = 1 ]
 then
